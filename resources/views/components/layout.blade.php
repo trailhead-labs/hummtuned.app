2b8ef08f9185239b5fda18fa2534a66d5dbefa5c
gwleuverink <willem@leuver.ink>
2026-07-13 01:27:33 +0200

    cache-bust favicons

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -17,10 +17,10 @@
         <meta name="description" content="{{ $description }}" />
     @endif
 
-    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any" />
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32.png') }}" />
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16.png') }}" />
-    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ filemtime(public_path('favicon.ico')) }}" sizes="any" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32.png') }}?v={{ filemtime(public_path('favicon-32.png')) }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16.png') }}?v={{ filemtime(public_path('favicon-16.png')) }}" />
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}?v={{ filemtime(public_path('apple-touch-icon.png')) }}" />
 
     @fonts
     @vite(['resources/css/app.css', 'resources/js/app.js'])
